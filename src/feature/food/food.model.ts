6590f220b6c42053ab86f81faf4162b6713df4e4
choisangwon <rngus3698@naver.com>
2025-01-24 17:19:26 +0900

    메뉴 모듈 추가

---
+++ src/feature/food/food.model.ts
@@ -0,0 +1,34 @@
+import { Injectable } from '@nestjs/common';
+import { DatabaseService } from '../../shared/database/database.service';
+import { PoolConnection } from 'mysql2/promise';
+
+@Injectable()
+export class FoodModel {
+  constructor(private readonly databaseService: DatabaseService) {}
+
+  /**
+   * 메뉴 카테고리 목록 조회
+   * @param connection
+   * @param storepkey
+   */
+  async getFoodCategoryList(connection: PoolConnection, storepkey: number) {
+    return await this.databaseService.dbQuery(
+      connection,
+      `select * from foodcategory where storepkey=? and useyn='Y' order by sort`,
+      [storepkey],
+    );
+  }
+
+  /**
+   * 메뉴 카테고리별 메뉴 목록 조회
+   * @param connection
+   * @param foodcategorypkey
+   */
+  async getFoodList(connection: PoolConnection, foodcategorypkey: number) {
+    return await this.databaseService.dbQuery(
+      connection,
+      `select * from food where foodcategorypkey=? and showyn='Y' order by sort`,
+      [foodcategorypkey],
+    );
+  }
+}
